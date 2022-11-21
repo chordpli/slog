f983b0e590f82604e82e029c74d218707dad0cdb
chordpli <chordplaylist@gmail.com>
2022-11-21 23:41:10 +0900

    feat: add user service

---
+++ src/main/java/com/slog/service/UserService.java
@@ -0,0 +1,14 @@
+package com.slog.service;
+
+import com.slog.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}

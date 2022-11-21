6ed830e4823a5f1972b417d6f8af097bbd04d0a3
chordpli <chordplaylist@gmail.com>
2022-11-21 23:44:40 +0900

    feat: add constructor

--- src/main/java/com/slog/controller/UserController.java
+++ src/main/java/com/slog/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.slog.controller;
 
+import com.slog.service.UserService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }

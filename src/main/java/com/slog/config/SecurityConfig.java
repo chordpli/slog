5bb175fbf89fa91b5236c8f6fadf91c19f0dbd07
chordpli <chordplaylist@gmail.com>
2022-12-11 23:32:17 +0900

    feat(config): securityconfig 수정
    1. securityFilterChain 내용 수정

--- src/main/java/com/slog/config/SecurityConfig.java
+++ src/main/java/com/slog/config/SecurityConfig.java
@@ -1,32 +1,41 @@
 package com.slog.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.slog.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig  {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+    private final MemberService memberService;
 
-
-
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.
+                httpBasic().disable()// http basic이 아닌 token으로 인증할 것 이기 때문에 disable 처리
+                .csrf().disable()
+                .cors().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/members/join", "/api/v1/members/login").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/v1/**").authenticated() // /api/v1/** 으로 넘어오는 post 요청들에 대해 인증요청. 순서대로 넘어오기 때문에 위에서 막히면 다 막혀버림. 순서 잘 고려해서 작성.
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtFilter(memberService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }

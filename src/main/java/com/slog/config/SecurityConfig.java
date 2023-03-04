e651434a0706b47e4018fbdf718082c6be2591b1
chordpli <chordplaylist@gmail.com>
2023-03-04 18:55:42 +0900

    fix: 403
    1. encoding 문제 수정
    2. get mapping permitAll

--- src/main/java/com/slog/config/SecurityConfig.java
+++ src/main/java/com/slog/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -34,6 +35,8 @@ public class SecurityConfig {
 		http
 			.csrf().disable()
 			.authorizeHttpRequests()
+			.antMatchers(HttpMethod.GET,"/**")
+			.permitAll()
 			.antMatchers("/**/auth/**")
 			.permitAll()
 			.anyRequest()

f40ba6e56f4e554b019936dfd2c58fe5a90530c7
chordpli <chordplaylist@gmail.com>
2023-03-06 02:05:51 +0900

    fix: docker run
    1. 도커파일 수정

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Download Docker image from S3 bucket
-aws s3 cp s3://my-work-project/slog-${{ github.sha }}.tar.gz .
+# (1) Docker 이미지를 S3에서 다운로드
+aws s3 cp s3://$S3_BUCKET_NAME/slog-${{ github.sha }}.tar.gz slog-${{ github.sha }}.tar.gz
 
-# Load Docker image into local Docker registry
+# (2) Docker 이미지를 로드
 docker load < slog-${{ github.sha }}.tar.gz
 
-# Stop and remove any existing containers
-docker stop slog
-docker rm slog
+# (3) 이전에 실행중인 컨테이너가 있다면 삭제
+docker stop slog || true
+docker rm slog || true
 
-# Run a new container using the newly loaded Docker image
-docker run -d -p 8080:8080 --name slog slog:${{ github.sha }}
+# (4) Docker 컨테이너 실행
+docker run -d --name slog -p 8080:8080 slog:${{ github.sha }}

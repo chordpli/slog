e2b741558e62fa6e2093b0570abb343bc364eee0
chordpli <chordplaylist@gmail.com>
2023-03-15 23:00:42 +0900

    feat: modified member, memberStatus
    1. memberStatus에 boolean 값을 추가
    - 중복 코드 제거, 응집도 증가, 재사용성 증가, 변경 관리 용이

--- src/main/java/com/slog/domain/enums/MemberStatus.java
+++ src/main/java/com/slog/domain/enums/MemberStatus.java
@@ -1,5 +1,32 @@
 package com.slog.domain.enums;
 
 public enum MemberStatus {
-	ADMIN, USER
+	ADMIN("ROLE_ADMIN"),
+	USER("ROLE_USER");
+
+	private final String role;
+
+	MemberStatus(String role) {
+		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	public boolean isEnabled() {
+		return true;
+	}
 }

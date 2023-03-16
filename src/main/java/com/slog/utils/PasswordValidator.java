b25e0982a26e16c030e65e6b3b543cd68b09eb50
chordpli <chordplaylist@gmail.com>
2023-03-16 23:36:34 +0900

    feat: add password validator
    1. 비밀번호 유효성 검사 클래스 생성

---
+++ src/main/java/com/slog/utils/PasswordValidator.java
@@ -0,0 +1,15 @@
+package com.slog.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+	private static final String PASSWORD_PATTERN =
+		"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+	public static boolean isValid(String password) {
+		Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+	}
+}

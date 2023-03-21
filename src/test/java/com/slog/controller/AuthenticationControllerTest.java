dd68427b4f5128519531cafaceb4b7136060bf7b
chordpli <chordplaylist@gmail.com>
2023-03-21 23:55:26 +0900

    feat: spring rest docs test

--- src/test/java/com/slog/controller/AuthenticationControllerTest.java
+++ src/test/java/com/slog/controller/AuthenticationControllerTest.java
@@ -1,18 +1,29 @@
 package com.slog.controller;
 
 import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.slog.config.JwtUtils;
@@ -25,6 +36,8 @@ import com.slog.repository.MemberRepository;
 import com.slog.service.MemberService;
 
 @WebMvcTest(AuthenticationController.class)
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
 @WithMockUser
 class AuthenticationControllerTest {
 	@Autowired
@@ -41,6 +54,13 @@ class AuthenticationControllerTest {
 
 	@MockBean
 	JwtUtils jwtUtils;
+
+	@BeforeEach
+	void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.apply(documentationConfiguration(restDocumentation))
+			.build();
+	}
 
 	@Test
 	void join_Success() throws Exception {
@@ -67,7 +87,22 @@ class AuthenticationControllerTest {
 			.andExpect(jsonPath("$.data.memberNo").value(1L))
 			.andExpect(jsonPath("$.data.memberEmail").value("test@email.com"))
 			.andExpect(jsonPath("$.data.memberNickname").value("test"))
-			.andDo(print());
+			.andDo(print())
+			.andDo(document("join-success",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("memberEmail").description("회원 이메일"),
+					fieldWithPath("memberNickname").description("회원 닉네임"),
+					fieldWithPath("memberPassword").description("회원 비밀번호")
+				),
+				responseFields(
+					fieldWithPath("resultCode").description("결과 코드"),
+					fieldWithPath("data.memberNo").description("회원 번호"),
+					fieldWithPath("data.memberEmail").description("회원 이메일"),
+					fieldWithPath("data.memberNickname").description("회원 닉네임")
+				)));
+		;
 	}
 
 	@Test
@@ -86,7 +121,20 @@ class AuthenticationControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(request)))
 			.andExpect(status().isConflict())
-			.andDo(print());
+			.andDo(print())
+			.andDo(document("join-fail-duplicated-member",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("memberEmail").description("회원 이메일"),
+					fieldWithPath("memberNickname").description("회원 닉네임"),
+					fieldWithPath("memberPassword").description("회원 비밀번호")
+				),
+				responseFields(
+					fieldWithPath("resultCode").description("결과 코드"),
+					fieldWithPath("data.errorCode").description("에러 코드"),
+					fieldWithPath("data.message").description("에러 메시지")
+				)));
 	}
 
 	@Test
@@ -107,7 +155,18 @@ class AuthenticationControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.resultCode").value("SUCCESS"))
 			.andExpect(jsonPath("$.data").value(token))
-			.andDo(print());
+			.andDo(print())
+			.andDo(document("authenticate-success",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("email").description("이메일"),
+					fieldWithPath("password").description("비밀번호")
+				),
+				responseFields(
+					fieldWithPath("resultCode").description("결과 코드"),
+					fieldWithPath("data").description("JWT 토큰")
+				)));
 	}
 
 	@Test
@@ -117,13 +176,26 @@ class AuthenticationControllerTest {
 			.password("Password123$")
 			.build();
 
-		given(memberService.authentication(any())).willThrow(new SlogAppException(ErrorCode.INCONSISTENT_INFORMATION, ErrorCode.INCONSISTENT_INFORMATION.getMessage()));
+		given(memberService.authentication(any())).willThrow(
+			new SlogAppException(ErrorCode.INCONSISTENT_INFORMATION, ErrorCode.INCONSISTENT_INFORMATION.getMessage()));
 
 		mockMvc.perform(post("/api/v1/auth/authenticate")
 				.with(csrf())
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(request)))
 			.andExpect(status().isConflict())
-			.andDo(print());
+			.andDo(print())
+			.andDo(document("authenticate-fail-inconsistent-information",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("email").description("이메일"),
+					fieldWithPath("password").description("비밀번호")
+				),
+				responseFields(
+					fieldWithPath("resultCode").description("결과 코드"),
+					fieldWithPath("data.errorCode").description("에러 코드"),
+					fieldWithPath("data.message").description("에러 메시지")
+				)));
 	}
 }
